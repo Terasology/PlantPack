2f9e95b602366e914ce86b32af9c520cea5e667f
MarcinSc <marcins78@gmail.com>
2014-02-15 17:06:30 +0000

    Refactoring of the generation and added additional growth and spawn conditions to CropGrowthDefinition.

--- src/main/java/org/terasology/plantPack/crop/CornGrowthDefinition.java
+++ src/main/java/org/terasology/plantPack/crop/CornGrowthDefinition.java
@@ -1,5 +1,8 @@
 package org.terasology.plantPack.crop;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import org.terasology.anotherWorld.LocalParameters;
 import org.terasology.gf.crop.CropGrowthDefinition;
 import org.terasology.world.block.BlockUri;
 import org.terasology.world.generator.plugin.RegisterPlugin;
@@ -17,6 +20,19 @@ public class CornGrowthDefinition extends CropGrowthDefinition {
         super(ID, Arrays.asList(
                 new BlockUri("PlantPack", "Corn1"), new BlockUri("PlantPack", "Corn2"), new BlockUri("PlantPack", "Corn3"),
                 new BlockUri("PlantPack", "Corn4"), new BlockUri("PlantPack", "Corn5"), new BlockUri("PlantPack", "Corn6"),
-                new BlockUri("PlantPack", "Corn7")), 120 * 1000);
+                new BlockUri("PlantPack", "Corn7")), 10 * 1000,
+                new Predicate<LocalParameters>() {
+                    @Override
+                    public boolean apply(LocalParameters input) {
+                        return input.getHumidity() > 0.2f && input.getTemperature() > 0.4f;
+                    }
+                },
+                new Function<LocalParameters, Float>() {
+                    @Override
+                    public Float apply(LocalParameters input) {
+                        return input.getHumidity();
+                    }
+                }
+        );
     }
 }
