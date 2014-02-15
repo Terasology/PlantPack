dc5c6a1a51b85a7c4a6d3810df2aa6420445dcc3
MarcinSc <marcins78@gmail.com>
2014-02-15 17:51:33 +0000

    Added rice generation/growth.

---
+++ src/main/java/org/terasology/plantPack/crop/RiceGrowthDefinition.java
@@ -0,0 +1,37 @@
+package org.terasology.plantPack.crop;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import org.terasology.anotherWorld.LocalParameters;
+import org.terasology.gf.crop.CropGrowthDefinition;
+import org.terasology.world.block.BlockUri;
+import org.terasology.world.generator.plugin.RegisterPlugin;
+
+import java.util.Arrays;
+
+/**
+ * @author Marcin Sciesinski <marcins78@gmail.com>
+ */
+@RegisterPlugin
+public class RiceGrowthDefinition extends CropGrowthDefinition {
+    public static final String ID = "PlantPack:rice";
+
+    public RiceGrowthDefinition() {
+        super(ID, Arrays.asList(
+                new BlockUri("PlantPack", "Rice1"), new BlockUri("PlantPack", "Rice2"), new BlockUri("PlantPack", "Rice3"),
+                new BlockUri("PlantPack", "Rice4")), 10 * 1000,
+                new Predicate<LocalParameters>() {
+                    @Override
+                    public boolean apply(LocalParameters input) {
+                        return input.getHumidity() > 0.4f && input.getTemperature() > 0.4f;
+                    }
+                },
+                new Function<LocalParameters, Float>() {
+                    @Override
+                    public Float apply(LocalParameters input) {
+                        return input.getHumidity();
+                    }
+                }
+        );
+    }
+}
