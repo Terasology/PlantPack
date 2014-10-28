be3712ec96932aad6095d1ba4fe1354d832e21f2
Marcin Sciesinski <marcins78@gmail.com>
2014-10-27 22:00:43 -0800

    Additional settings for plant growth.

--- src/main/java/org/terasology/plantPack/crop/CornGrowthDefinition.java
+++ src/main/java/org/terasology/plantPack/crop/CornGrowthDefinition.java
@@ -3,9 +3,10 @@ package org.terasology.plantPack.crop;
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import org.terasology.anotherWorld.LocalParameters;
-import org.terasology.gf.grass.ReplaceBlockGrowthDefinition;
+import org.terasology.gf.grass.AdvancedStagesGrowthDefinition;
 import org.terasology.world.block.BlockUri;
 import org.terasology.world.generator.plugin.RegisterPlugin;
+import org.terasology.world.time.WorldTimeImpl;
 
 import java.util.Arrays;
 
@@ -13,32 +14,46 @@ import java.util.Arrays;
  * @author Marcin Sciesinski <marcins78@gmail.com>
  */
 @RegisterPlugin
-public class CornGrowthDefinition extends ReplaceBlockGrowthDefinition {
+public class CornGrowthDefinition extends AdvancedStagesGrowthDefinition {
     public static final String ID = "PlantPack:corn";
 
     public CornGrowthDefinition() {
-        super(ID, Arrays.asList(
-                new BlockUri("PlantPack", "Corn1"), new BlockUri("PlantPack", "Corn2"), new BlockUri("PlantPack", "Corn3"),
-                new BlockUri("PlantPack", "Corn4"), new BlockUri("PlantPack", "Corn5"), new BlockUri("PlantPack", "Corn6"),
-                new BlockUri("PlantPack", "Corn7")), 10 * 1000,
+        super(ID,
                 new Predicate<LocalParameters>() {
                     @Override
                     public boolean apply(LocalParameters input) {
                         return input.getHumidity() > 0.2f && input.getTemperature() > 10f;
                     }
                 },
-                new Function<LocalParameters, Float>() {
+                new Function<LocalParameters, Long>() {
                     @Override
-                    public Float apply(LocalParameters input) {
-                        return 0.2f * input.getHumidity();
+                    public Long apply(LocalParameters input) {
+                        // Corn growth depends on temperature
+                        float temperature = input.getTemperature();
+                        long yearLength = 24 * WorldTimeImpl.DAYS_TO_MS;
+                        float minGrowthLength = yearLength / 7f;
+                        int stageCount = 7;
+                        float minStageGrowthLength = minGrowthLength / (stageCount - 1);
+                        float maxTemperature = 30;
+                        float minTemperature = 10;
+                        float maxMultiplier = 2;
+                        if (temperature >= maxTemperature) {
+                            return (long) minStageGrowthLength;
+                        } else if (temperature <= minTemperature) {
+                            return (long) (minStageGrowthLength * maxMultiplier);
+                        } else {
+                            return (long) ((1 + 1f * (maxMultiplier - 1) * (maxTemperature - temperature) / (maxTemperature - minTemperature)) * minStageGrowthLength);
+                        }
                     }
-                }
-        );
-        setDeathCondition(
+                },
+                Arrays.asList(
+                        new BlockUri("PlantPack", "Corn1"), new BlockUri("PlantPack", "Corn2"), new BlockUri("PlantPack", "Corn3"),
+                        new BlockUri("PlantPack", "Corn4"), new BlockUri("PlantPack", "Corn5"), new BlockUri("PlantPack", "Corn6"),
+                        new BlockUri("PlantPack", "Corn7")),
                 new Predicate<LocalParameters>() {
                     @Override
                     public boolean apply(LocalParameters input) {
-                        return input.getHumidity()<0.2f || input.getTemperature() < 0f;
+                        return input.getHumidity() < 0.2f || input.getTemperature() < 0f;
                     }
                 }, new BlockUri("PlantPack", "DeadBush"));
     }
--- src/main/java/org/terasology/plantPack/crop/RiceGrowthDefinition.java
+++ src/main/java/org/terasology/plantPack/crop/RiceGrowthDefinition.java
@@ -3,9 +3,10 @@ package org.terasology.plantPack.crop;
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import org.terasology.anotherWorld.LocalParameters;
-import org.terasology.gf.grass.ReplaceBlockGrowthDefinition;
+import org.terasology.gf.grass.AdvancedStagesGrowthDefinition;
 import org.terasology.world.block.BlockUri;
 import org.terasology.world.generator.plugin.RegisterPlugin;
+import org.terasology.world.time.WorldTimeImpl;
 
 import java.util.Arrays;
 
@@ -13,31 +14,45 @@ import java.util.Arrays;
  * @author Marcin Sciesinski <marcins78@gmail.com>
  */
 @RegisterPlugin
-public class RiceGrowthDefinition extends ReplaceBlockGrowthDefinition {
+public class RiceGrowthDefinition extends AdvancedStagesGrowthDefinition {
     public static final String ID = "PlantPack:rice";
 
     public RiceGrowthDefinition() {
-        super(ID, Arrays.asList(
-                new BlockUri("PlantPack", "Rice1"), new BlockUri("PlantPack", "Rice2"), new BlockUri("PlantPack", "Rice3"),
-                new BlockUri("PlantPack", "Rice4")), 10 * 1000,
+        super(ID,
                 new Predicate<LocalParameters>() {
                     @Override
                     public boolean apply(LocalParameters input) {
                         return input.getHumidity() > 0.4f && input.getTemperature() > 15f;
                     }
                 },
-                new Function<LocalParameters, Float>() {
+                new Function<LocalParameters, Long>() {
                     @Override
-                    public Float apply(LocalParameters input) {
-                        return 0.2f * input.getHumidity();
+                    public Long apply(LocalParameters input) {
+                        // Corn growth depends on humidity
+                        float humidity = input.getHumidity();
+                        long yearLength = 24 * WorldTimeImpl.DAYS_TO_MS;
+                        float minGrowthLength = yearLength / 3f;
+                        int stageCount = 4;
+                        float minStageGrowthLength = minGrowthLength / (stageCount - 1);
+                        float maxTemperature = 0.9f;
+                        float minTemperature = 0.6f;
+                        float maxMultiplier = 1.5f;
+                        if (humidity >= maxTemperature) {
+                            return (long) minStageGrowthLength;
+                        } else if (humidity <= minTemperature) {
+                            return (long) (minStageGrowthLength * maxMultiplier);
+                        } else {
+                            return (long) ((1 + 1f * (maxMultiplier - 1) * (maxTemperature - humidity) / (maxTemperature - minTemperature)) * minStageGrowthLength);
+                        }
                     }
-                }
-        );
-        setDeathCondition(
+                },
+                Arrays.asList(
+                        new BlockUri("PlantPack", "Rice1"), new BlockUri("PlantPack", "Rice2"), new BlockUri("PlantPack", "Rice3"),
+                        new BlockUri("PlantPack", "Rice4")),
                 new Predicate<LocalParameters>() {
                     @Override
                     public boolean apply(LocalParameters input) {
-                        return input.getHumidity()<0.4f || input.getTemperature() < 5f;
+                        return input.getHumidity() < 0.4f || input.getTemperature() < 5f;
                     }
                 }, new BlockUri("PlantPack", "DeadBush"));
     }
